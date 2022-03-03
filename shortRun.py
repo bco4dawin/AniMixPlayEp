b0b61ae5508c16269a895dc3743aafb301ed27b8
bco4dawin <98175420+bco4dawin@users.noreply.github.com>
2022-03-03 10:01:05 -0500

    Update shortRun.py

--- shortRun.py
+++ shortRun.py
@@ -1,5 +1,8 @@
 from requests import get
 import time, os, math
+import sys
+
+sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=50, cols=100))
 url = "https://raw.githubusercontent.com/bco4dawin/AniMixPlayEp/main/main.py"
 string = get(url).text
 
@@ -10,7 +13,7 @@ while True:
         r = 30
         while x < r:
             print("\033[A                             \033[A")
-            print(f'     Updating ... {("█" * (math.ceil((x/r) * 20) + 1)) + ("▒" * math.ceil(20 - ((x/r) * 20)))}')
+            print(f'     {r - x}s Updating ... {("█" * (math.ceil((x/r) * 20) + 1)) + ("▒" * math.ceil(20 - ((x/r) * 20)))}')
             time.sleep(1)
             x+=1
     except KeyboardInterrupt:
